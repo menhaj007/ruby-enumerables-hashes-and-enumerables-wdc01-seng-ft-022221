67dedc5c6d7e99ecf58e89fead99c528f478cc4f
witty-trace-1906 <menhajuddin.sharaf@gmail.com>
2020-12-28 15:55:02 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,13 +8,8 @@ suite_e: "Crumpet the Elf"
 }
 
 
-# def select_winner(passengers)
-#   # add the code snippet here!
+def select_winner(passengers)
+  # add the code snippet here!
   
-# end
-
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
-  end
 end
+
